f9a58b6d9bbaea4122b96e184f07e536dcdc7fe3
gpekd5 <gpekd5@naver.com>
2026-04-23 20:27:38 +0900

    add: 초기 세팅

---
+++ src/main/java/com/example/ecommercebackofficeproject/EcommerceBackOfficeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommercebackofficeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBackOfficeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceBackOfficeProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ecommercebackofficeproject/EcommerceBackOfficeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommercebackofficeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceBackOfficeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
