77703fb68e7427e1ea88f846b62ead09151817c1
gpekd5 <gpekd5@naver.com>
2026-04-24 19:36:37 +0900

    feat: 관리자 회원가입 기능 구현 fix: properties Mysql 주소 수정

--- src/main/java/com/example/ecommercebackofficeproject/admin/controller/AdminController.java
+++ src/main/java/com/example/ecommercebackofficeproject/admin/controller/AdminController.java
@@ -1,13 +1,27 @@
 package com.example.ecommercebackofficeproject.admin.controller;
 
+import com.example.ecommercebackofficeproject.admin.dto.request.SignupAdminRequestDto;
+import com.example.ecommercebackofficeproject.admin.dto.response.SignupAdminResponseDto;
 import com.example.ecommercebackofficeproject.admin.service.AdminService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/admins")
 public class AdminController {
 
     private final AdminService adminService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<SignupAdminResponseDto> signupAdmin(@Valid @RequestBody SignupAdminRequestDto request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(adminService.signupAdmin(request));
+    }
 
 }
