f819540b21bf9f55b51136755023daaace8a7720
devdong1231 <79241089+devdong1231@users.noreply.github.com>
2026-04-24 19:58:39 +0900

    feat: 주문 생성 API 기본 흐름 구현
    - Create Order API 기본 흐름 구현
    - 생성자에 @Builder annotation 적용(Order.java, CreateOrderResponse.java)
    - 예외 처리 미완료
    - 일부 의존 클래스 미구현 상태
    - 주문번호 동시성 처리는 추후 개선 예정

--- src/main/java/com/example/ecommercebackofficeproject/order/service/OrderService.java
+++ src/main/java/com/example/ecommercebackofficeproject/order/service/OrderService.java
@@ -1,5 +1,7 @@
 package com.example.ecommercebackofficeproject.order.service;
 
+import com.example.ecommercebackofficeproject.admin.entity.Admin;
+import com.example.ecommercebackofficeproject.admin.repository.AdminRepository;
 import com.example.ecommercebackofficeproject.customer.entity.Customer;
 import com.example.ecommercebackofficeproject.customer.repository.CustomerRepository;
 import com.example.ecommercebackofficeproject.order.dto.CreateOrderRequest;
@@ -8,6 +10,7 @@ import com.example.ecommercebackofficeproject.order.entity.Order;
 import com.example.ecommercebackofficeproject.order.repository.OrderRepository;
 import com.example.ecommercebackofficeproject.product.entity.Product;
 import com.example.ecommercebackofficeproject.product.repository.ProductRepository;
+import com.example.ecommercebackofficeproject.product.type.ProductStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,16 +25,29 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final ProductRepository productRepository;
     private final CustomerRepository customerRepository;
+    private final AdminRepository adminRepository;
 
     @Transactional
     public CreateOrderResponse createOrder(Long adminId, CreateOrderRequest request) {
-        // todo - 단종 / 품절 / 재고 부족 / 고객 없음 / 상품 없음 / 401 / 403 처리
+        // todo - 단종 / 품절 / 재고 부족 / 고객 없음 / 상품 없음 / 403 처리
+
+        // todo - 403 처리
         Product product = productRepository.findById(request.getProductId()).orElseThrow(
                 () -> new IllegalStateException("상품을 찾을 수 없습니다.") // todo - 상품 없음 에러 처리
         );
         Customer customer = customerRepository.findById(request.getCustomerId()).orElseThrow(
                 () -> new IllegalStateException("고객을 찾을 수 없습니다.") // todo - 고객 없음 에러 처리
         );
+        if(product.getStatus() == ProductStatus.DISCONTINUED){ // todo - 단종된 상품 에러 처리
+            throw new IllegalArgumentException("단종된 상품은 주문할 수 없습니다.");
+        }
+        else if(product.getStatus() == ProductStatus.SOLD_OUT){ // todo - 품절된 상품 에러 처리
+            throw new IllegalArgumentException("품절된 상품은 주문할 수 없습니다.");
+        }
+        if(request.getQuantity()>product.getStock()){
+            throw new IllegalArgumentException("상품 재고가 부족합니다."); // todo - 재고 부족 에러 처리
+        }
+
 
         Order order = Order.builder()
                 .orderNumber(createOrderNumber())
@@ -40,6 +56,8 @@ public class OrderService {
                 .quantity(request.getQuantity())
                 .createdByAdminId(adminId)
                 .build();
+
+        // todo - 상품 재고 update
 
         orderRepository.save(order);
 
@@ -72,13 +90,5 @@ public class OrderService {
 
         return orderNumber;
     }
-
-    // todo - 401 처리 메서드 작성
-
-    // todo - 403 처리 메서드 작성
-
-    // todo - 고객 404 처리 메서드 작성
-
-    // todo - 상품 404 처리 메서드 작성
 
 }
